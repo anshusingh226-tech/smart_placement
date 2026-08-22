e37da0dd1396333cf6a87c4f448ab33b6a60493d
Tamanna <roytamanna0812@gmail.com>
2026-08-22 21:47:50 +0530

    Update login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AdminLayout from './layouts/AdminLayout'
-import AdminDashboard from './pages/admin/AdminDashboard'
-import UserManagement from './pages/admin/UserManagement'
-import SkillsAssessments from './pages/admin/SkillsAssessments'
-import Companies from './pages/admin/Companies'
+import LoginPage from "./pages/auth/LoginPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="users" element={<UserManagement />} />
-          <Route
-            path="skills-assessments"
-            element={<SkillsAssessments />}
-          />
-          <Route path="companies" element={<Companies />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <LoginPage />;
 }
 
-export default App
+export default App;
